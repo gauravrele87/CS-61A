4925edf20a3cf9d8a511f16b781964277760935a
Gaurav Rele <relegaurav87@gmail.com>
2018-09-20 16:59:24 -0700

    Added ants and lab06 to the repo

---
+++ lab06/lab06_extra.py
@@ -0,0 +1,27 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    x = 0
+    def vend():
+        nonlocal x,snacks
+        x = x + 1
+        return snacks[(x-1)%len(snacks)]
+    return vend
